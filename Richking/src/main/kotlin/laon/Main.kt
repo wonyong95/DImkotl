fbe2dc6c90d4c235e336f7311f3f5f8947fb3dae
DESKTOP-DF6RQ5T\2dawi <2dawit70@gmail.com>
2023-08-20 00:34:19 +0900

    type

---
+++ Richking/src/main/kotlin/laon/Main.kt
@@ -0,0 +1,17 @@
+package laon
+
+fun main(args: Array<String>) {
+    val name="윤원용"
+
+    val str=
+"""
+    ABC
+    ECF
+    ${name}
+""".trimIndent()
+    println(str)
+
+    val str1= "BCD"
+    println(str[0])
+    println(str[2])
+}
